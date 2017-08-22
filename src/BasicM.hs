3df49a3ce5097633dc458458da1795516fe9850a
tonyfloatersu <tonyfloater@gmail.com>
2017-08-22 20:22:17 +0800

    board show instance done

--- src/BasicM.hs
+++ src/BasicM.hs
@@ -12,30 +12,31 @@ instance Show PVEPlayer where
 data PVPPlayer = Human1 | Human2
                deriving ( Eq, Enum, Show )
 
-data Color = White | Black | NULL
-           deriving ( Eq, Enum )
+data Color = White | Black | NULL | Next
+             deriving ( Eq, Enum )
 
 instance Show Color where
-    show c    = [ "O", "X", "#" ] !! fromEnum c
+    show c    = [ "O", "X", "#", "*" ] !! fromEnum c
 
 negnate :: Color -> Color
 negnate c | c == White    = Black
           | c == NULL     = NULL
+          | c == Next     = Next
           | otherwise     = White
 
 type Location = ( Int, Int )
 
 data Hand = Hand { loc :: Location
-                 , color :: Color }
+                 , clr :: Color }
 
 instance Eq Hand where
-    Hand { loc = l1, color = c1 } == Hand { loc = l2, color = c2 }    = l1 == l2 && c1 == c2
+    Hand { loc = l1, clr = c1 } == Hand { loc = l2, clr = c2 }    = l1 == l2 && c1 == c2
 
 instance Show Hand where
-    show Hand { loc = l, color = c }    = "(" ++ concat ([ show . fst
-                                                         , const ", "
-                                                         , show . snd ] <*> [l])
-                                          ++ ", " ++ show c ++ ")"
+    show Hand { loc = l, clr = c }    = "(" ++ concat ([ show . fst
+                                                       , const ", "
+                                                       , show . snd ] <*> [l])
+                                        ++ ", " ++ show c ++ ")"
 
 prompt :: String -> (String -> Bool) -> IO String
 prompt command judgef    = putStrLn command >> getLine >>=
@@ -55,7 +56,7 @@ noChooseHint    = "\nIn PVP mode you need to decide who goes first; \
 
 {- we insert new hands into the front -}
 
-type Hands = [ Hand ]
+type Hands = [Hand]
 
 type HumanHands = Hands
 
@@ -67,22 +68,22 @@ type Human2Hands = Hands
 
 data Operations = Oper Hands Hands
 
-{- Let the empty hand for no place to go as ((-1, -1), ) -}
+{- Let the empty hand for no place to go as ((-1, -1), #) -}
 
 showOp :: Operations -> String
-showOp (Oper h1 h2) | null h1 && null h2    = []
-                    | null h1               = "\t\t" ++ (show . head) h2 ++ "\n"
-                                              ++ showOp (Oper h1 (tail h2))
-                    | null h2               = (show . head) h1 ++ "\t\t\n"
-                                              ++ showOp (Oper (tail h1) h2)
-                    | otherwise             = (show . head) h1 ++ "\t\t" ++ (show . head) h2
-                                              ++ "\n" ++ showOp (Oper (tail h1) (tail h2))
+showOp (Oper h a) | null h && null a    = []
+                  | null h              = "\t\t" ++ (show . head) a ++ "\n"
+                                          ++ showOp (Oper h (tail a))
+                  | null a              = (show . head) h ++ "\t\t\n"
+                                          ++ showOp (Oper (tail h) a)
+                  | otherwise           = (show . head) h ++ "\t\t" ++ (show . head) a
+                                          ++ "\n" ++ showOp (Oper (tail h) (tail a))
 
 instance Show Operations where
-    show (Oper h1 h2)    = "P1\t\t\t\tP2\n" ++ showOp (Oper (reverse h1) (reverse h2))
+    show (Oper h a)    = "P1\t\t\t\tP2\n" ++ showOp (Oper (reverse h) (reverse a))
 
 backWardRegret :: Operations -> Operations
-backWardRegret (Oper h1 h2) | null h1 && null h2    = Oper [] []
-                            | null h1               = Oper [] (tail h2)
-                            | null h2               = Oper (tail h1) []
-                            | otherwise             = Oper (tail h1) (tail h2)
+backWardRegret (Oper h a) | null h && null a    = Oper [] []
+                          | null h              = Oper [] (tail a)
+                          | null a              = Oper (tail h) []
+                          | otherwise           = Oper (tail h) (tail a)
--- src/BoardM.hs
+++ src/BoardM.hs
@@ -2,3 +2,26 @@ module BoardM where
 
 import           BasicM
 
+type Block = Hand
+
+newtype Line = Line [Block]
+
+newtype Board = Board [Line]
+
+boardLine :: String
+boardLine    = "+---+---+---+---+---+---+---+---+\n"
+
+instance Show Line where
+    show (Line b)    = concat ((\Hand { loc = _, clr = c } -> "| " ++ show c ++ " ") <$> b)
+                       ++ "|\n"
+
+instance Show Board where
+    show (Board l)    = concat ((\x -> boardLine ++ show x) <$> l) ++ boardLine
+
+{- just used for test, not for real usage -}
+
+blankLine :: Line
+blankLine    = Line $ replicate size Hand { loc = (1, 2), clr = NULL }
+
+blankBoard :: Board
+blankBoard    = Board $ replicate size blankLine
