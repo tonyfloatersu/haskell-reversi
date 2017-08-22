d1bbbcc454a7d6846f4516cd713a8181b7f101a4
tonyfloatersu <tonyfloater@gmail.com>
2017-08-23 01:25:54 +0800

    add container dependency

--- src/BoardM.hs
+++ src/BoardM.hs
@@ -1,6 +1,7 @@
 module BoardM where
 
 import           BasicM
+import           Control.Monad.ST
 
 type Block = Hand
 
@@ -25,3 +26,16 @@ blankLine    = Line $ replicate size Hand { loc = (1, 2), clr = NULL }
 
 blankBoard :: Board
 blankBoard    = Board $ replicate size blankLine
+
+searchPriority :: (Int, Int) -> Int
+searchPriority (row, col)    = ((!! reverseData row) . (!! reverseData col))
+                               [ [100, -5, 10, 5]
+                               , [-5, -50, 1, 1]
+                               , [10, 1, 3, 2]
+                               , [5, 1, 2, 1] ]
+    where reverseData :: Int -> Int
+          reverseData x    = if x > 3 then 7 - x else x
+
+dirVec :: [(Int, Int)]
+dirVec    = [ (-1, -1), (-1, 1), (-1, 0), (1, 0)
+            , (1, 1), (1, -1), (0, 1), (0, -1) ]
