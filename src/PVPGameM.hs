ded50a7d817f13f25db2c3ec54039e670f6aaad4
tonyfloatersu <tonyfloater@gmail.com>
2017-09-01 15:09:10 +0800

    pvp with no regret done

--- src/PVPGameM.hs
+++ src/PVPGameM.hs
@@ -4,18 +4,28 @@ import           BoardM
 import           BasicM
 import qualified Data.Maybe as DM
 
-pvp :: Board -> IO ()
-pvp b | endingJudgement b    = return ()
-      | otherwise            = undefined
+pvpGame :: IO ()
+pvpGame    = (print . modifyTobe White $ boardInit) >> pvpFake boardInit
+
+pvpFake :: Board -> IO ()
+pvpFake b | endingJudgement b    = return ()
+          | otherwise            = fragment b >>= pvpFake
 
 {- first white then black -}
 
+fragment :: Board -> IO Board
+fragment b    = promptChessAndShow "give me a white one" White b
+                >>= (\x -> (print . modifyTobe Black $ x) >> return x)
+                >>= promptChessAndShow "give me a black one" Black
+                >>= (\x -> (print . modifyTobe White $ x) >> return x)
+
 promptChessAndShow :: String -> Color -> Board -> IO Board
 promptChessAndShow pt c b    = modfyJdg where
-    fallCond      = not . null $ colorForNext c b :: Bool
-    locaPrpt      = promptLocation pt $ nextMoveJudge b c :: IO Location
-    handFall      = (\l -> Hand { loc = l, clr = c }) <$> locaPrpt :: IO Hand
-    modfyJdg      = if fallCond then (`modifyH` b) <$> handFall else return b :: IO Board
+    fallCond    = not . null $ colorForNext c b                           :: Bool
+    locaPrpt    = promptLocation pt $ nextMoveJudge b c                   :: IO Location
+    handFall    = (\l -> Hand { loc = l, clr = c }) <$> locaPrpt          :: IO Hand
+    modfyJdg    = if fallCond then (`modifyH` b) <$> handFall
+                  else putStrLn "no location for you to fall" >> return b :: IO Board
 
 readLocationMaybe :: IO (Maybe Location)
 readLocationMaybe    = (fst <$>) . DM.listToMaybe
